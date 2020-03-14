9aa2460eced0d7f79d49a77b9008aa3fb927c8be
EvaLantsova <55274804+EvaLantsova@users.noreply.github.com>
2020-03-14 18:06:04 +0300

    animation+stickers

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,29 @@
-const $imgValue = document.getElementById('myImage');
+let starsNumber = 10
+let stickerStar = document.querySelector('.sticker_star')
 
-$imgValue.onclick = function () {
+for (let i = 0; i < 10; i++) {
+	let newStarImg = document.createElement('img')
+	newStarImg.classList.add('star')
+	newStarImg.src = 'img/star1.svg'
+	newStarImg.onclick = function() {
+		newStarImg.setAttribute('src', 'img/star2.svg');
+	}
+	newStarImg.style.transform = `translate(${anime.random(1,100)}vw, ${anime.random(1,100)}vh)`
+	stickerStar.append(newStarImg)
+}
 
-	$imgValue.setAttribute('src', 'img/star2.svg');
+let stars = document.querySelectorAll('.star')
+stars.forEach(function(element) {
+	element.onclick = function() {
+		element.setAttribute('src', 'img/star2.svg');
+	}
+})
 
-};
+let infiniteLoopAlternate = anime({
+  targets: '.pineapple',
+  translateX: 250,
+	translateY: -250,
+	duration: 2000,
+  direction: 'alternate',
+  loop: true
+});
